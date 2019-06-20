62ad56221e6d2968e926c03cd5454ebd9e5e47d9
IsaacSteadman <isaac.steadman@yahoo.com>
2019-06-19 22:34:43 -0500

    added testing

---
+++ test-client/test_dummy.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Dummy Client Test', () => {
+  it('should just pass', () => {
+    expect(true).to.equal(true, 'assert fail?');
+  });
+});
---
+++ test-server/test_dummy.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Dummy Server Test', () => {
+  it('should just pass', () => {
+    expect(true).to.equal(true, 'assert fail?');
+  });
+});
